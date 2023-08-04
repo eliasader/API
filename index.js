1b280a9ed3414d59af64538de68682d2003b4410
elisasader <elisasdnatividade18@gmail.com>
2023-08-03 22:05:25 -0300

    Adicionando funcionalidades para conexão com frontend

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-
 const express = require('express');
+const cors = require('cors')
 
 const server = express();
-const port = 3000;
-
+const port = 5000;
+server.use(cors());
 server.use(express.json());
 
-const pessoas = ['Elisa','Jonas','Fabio','Mariana','Maria','Julia'];
-
+const pessoas = [{'name':'Elias', 'age': '19'},
+            {'name':'Jonas','age': '13'},
+            {'name':'Mariana','age': '18'},
+            {'name':'Maria','age': '20'},
+            {'name':'Julia','age': '34'},
+            {'name': 'Fabio' ,'age': '55'}]
+            
 server.get('/:index', (req,res) =>{
     const { index } = req.params;
     return res.json(pessoas[index]);
@@ -25,12 +30,12 @@ server.post('/', (req, res) =>{
 server.put('/:index', (req,res) =>{
     const { index } = req.params;
     const { pessoa } = req.body;
-    pessoas[index] = pessoa
+    pessoas[index] = pessoa;
     return res.json(pessoas);
 })
 server.delete('/:index', (req,res) =>{
     const { index } = req.params;
-    pessoas.splice(index,1) 
+    pessoas.splice(index,1);
     return res.json(pessoas);
 })
 console.log(`servidor na porta: http://localhost:${port}`);
